0f458466cc13a57bba154ace90c0b1c5dc56890c
Gregory <gregoj1@usxxgisinjm1.corp.emc.com>
2013-10-22 20:06:27 -0700

    create fake network player that chooses a random move
    run the fake_network_player script in a separate
    console, then you can set an opponent to a network player
    and interact in the text GUI

--- amazons/player.py
+++ amazons/player.py
@@ -4,7 +4,8 @@ from board import WHITE, BLACK
 from invalid_move_error import InvalidMoveError
 from move import Move
 from random import randint
-
+from socket import *
+import pickle
 
 class ResignGame(Exception):
 	def __init__(self):
@@ -67,3 +68,26 @@ class AIPlayer(Player):
 		"""Temporary stand-in for AI opponent.  Pick a random valid move."""
 		valid_moves = game.board.get_valid_moves()
 		return valid_moves[randint(0, len(valid_moves) - 1)]
+
+class NetworkPlayer(Player):
+	def __init__(self, color, text_ui=False, **kwargs):
+		super(NetworkPlayer, self).__init__(color, text_ui)
+		if 'server' in kwargs:
+			self.server = kwargs['server']
+		else:
+			self.server = 'localhost'
+		if 'port' in kwargs:
+			self.port = kwargs['port']
+		else:
+			self.port = 60987
+	
+	def __str__(self):
+		return "Network player"
+	
+	def next_move_txt(self, game):
+		sock = socket(AF_INET, SOCK_STREAM)
+		sock.connect((self.server, self.port))
+		sock.send("/".join([str(x) for x in game.board.get_valid_moves()]))
+		move = sock.recv(4096)
+		print "Network player moves: %s" % move
+		return Move(move)
--- bin/fake_network_player.py
+++ bin/fake_network_player.py
@@ -1,5 +1,6 @@
 from socket import *
 import pickle
+from random import randint
 
 server_host = '' 
 server_port = 60987 # selected for no good reason
@@ -8,16 +9,23 @@ sock = socket(AF_INET, SOCK_STREAM)
 sock.bind((server_host,server_port))
 sock.listen(5)
 
-while 1:
-    conn, addr = sock.accept()
-    try:
-        data_in = ''
-        while 1:
-            data = conn.recv(1024)
-            if not data:
-                break
+try:
+    while 1:
+        conn, addr = sock.accept()
+        try:
+            data_in = ''
+            print "waiting to recv"
+            data = conn.recv(4096)
+            print "received from socket: %s <%d>" % (data, len(data))
             data_in += data
-        
-        print pickle.loads(data_in)
-    finally:
-        conn.close()
+            #board = pickle.loads(data + "\n")
+            valid_moves = data.split("/")
+            print len(valid_moves)
+            move = valid_moves[randint(0, len(valid_moves) - 1)]
+            print "sending" + move
+            conn.send(move)
+
+        finally:
+            conn.close()
+finally:
+    sock.close()
